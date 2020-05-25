9d1f5bef24908ef1e12b4f803c1925412600ef2e
saber <405543445@qq.com>
2020-05-25 18:00:46 +0800

    string reverse

--- src/kyu8/StringReverse.java
+++ src/kyu8/StringReverse.java
@@ -1,13 +1,21 @@
 package kyu8;
 
-import java.util.Collections;
 
 /**
  * https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/java
+ *
+ * 可以new StringBuilder(String str) 做参数, 然后直接反转. 返回.
  */
 public class StringReverse {
     public static String solution(String str) {
-        return "";
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder(str);   //更简单的方法
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sb.append(chars[i]);
+        }
+//        sb.reverse()
+        return sb.toString();
     }
 
 }
